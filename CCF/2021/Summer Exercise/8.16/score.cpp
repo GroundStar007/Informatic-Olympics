d125be029f2ec186ec72e89740ac898ea8b953c2
GroundStar007 <plokmn0719@163.com>
2024-08-27 02:46:20 +0800

    iii

--- CCF/2021/Summer Exercise/8.16/score.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    double a,b,c;
-    cin>>a>>b>>c;
-    cout<<0.2*a+0.3*b+0.5*c;
-    return 0;
-}
--- COMMON/xh.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-	for (int i = 1; i <= 99; i ++)
-        cout << "https://xhamster" << i << ".com" << endl;
-	return 0;
-}
